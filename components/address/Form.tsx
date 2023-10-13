da82efb2d6aaa05c66e7343a54ef3e35ff14f174
tadeogavensky <tadeogavensky.email@gmail.com>
2023-10-12 22:08:53 -0300

    select country state city

--- components/address/Form.tsx
+++ components/address/Form.tsx
@@ -1,8 +1,91 @@
+import axios from "axios";
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { IoClose } from "react-icons/io5";
+import {
+  Country,
+  State,
+  City,
+  ICity,
+  ICountry,
+  IState,
+} from "country-state-city";
 
-const Form = ({ closeForm }: { closeForm: () => void }) => {
+import Select from "react-select";
+
+type FormProps = {
+  closeForm: () => void;
+};
+
+const Form: React.FC<FormProps> = ({ closeForm }) => {
+  const countryData: ICountry[] = Country.getAllCountries();
+
+  const [values, setValues] = useState({
+    country: {
+      value: "Argentina",
+      isoCode: "",
+    },
+    state: {
+      value: "Buenos Aires",
+      isoCode: "",
+    },
+    city: {
+      value: "Balvanera",
+      stateCode: "",
+    },
+  });
+
+  const initialValues = {
+    state: {
+      value: "",
+      isoCode: "",
+    },
+    city: {
+      value: "",
+      stateCode: "",
+    },
+  };
+
+  const updatedCountries = countryData.map((country) => ({
+    label: country.name,
+    value: country.isoCode,
+    ...country,
+  }));
+
+  const updatedStates = (isoCode: string) => {
+    return State.getStatesOfCountry(isoCode).map((state) => ({
+      label: state.name,
+      value: state.isoCode,
+      ...state,
+    }));
+  };
+
+  const updatedCities = (countryCode: string, stateCode: string) => {
+    return City.getCitiesOfState(countryCode, stateCode).map((city) => ({
+      label: city.name,
+      value: city.stateCode,
+      ...city,
+    }));
+  };
+
+  const handleCountryChange = (selectedOption: any) => {
+    setValues({
+      ...values,
+      country: selectedOption,
+      ...initialValues,
+    });
+  };
+  const handleStateChange = (selectedOption: any) => {
+    console.log(selectedOption);
+
+    setValues({ ...values, state: selectedOption });
+  };
+
+  const handleCityChange = (selectedOption: any) => {
+    setValues({ ...values, city: selectedOption });
+  };
+
+  console.log("values :>> ", values);
   const container = {
     exit: {
       y: 500,
@@ -12,7 +95,6 @@ const Form = ({ closeForm }: { closeForm: () => void }) => {
       },
     },
   };
-
   return (
     <motion.div
       variants={container}
@@ -22,7 +104,7 @@ const Form = ({ closeForm }: { closeForm: () => void }) => {
       }}
       transition={{ duration: 0.3 }}
       exit={"exit"}
-      className="min-h-screen pt-6 p-4 bg-white absolute w-full flex flex-col justify-between top-0 left-0 z-50 overflow-hidden"
+      className="pt-6 p-4 min-h-screen justify-between bg-white absolute w-full flex flex-col top-0 left-0 z-50 overflow-hidden"
     >
       <div className="flex items-center justify-between ">
         <button onClick={closeForm} type="button">
@@ -31,14 +113,65 @@ const Form = ({ closeForm }: { closeForm: () => void }) => {
         <h1 className="font-bold font-heading text-xl">New Address</h1>
         <span></span>
       </div>
-      <form className="">
-        <button
-          className="w-full bg-primary text-white font-body font-semibold py-1 rounded-full mt-10"
-          type="submit"
-        >
-          Save
-        </button>
+      <form className="flex flex-col justify-start items-start w-full ">
+        <div className="flex flex-col w-full">
+          <label className="block text-base font-body font-semibold leading-6 text-gray-900">
+            ZIP/Postal Code
+          </label>
+        </div>
+        <div className="flex flex-col w-full">
+          <label className="block text-base font-body font-semibold leading-6 text-gray-900">
+            Country
+          </label>
+          <Select
+            id="country"
+            name="country"
+            options={updatedCountries}
+            onChange={handleCountryChange}
+          />
+        </div>
+        <div className="flex flex-col w-full">
+          {values.country && (
+            <div>
+              <label className="block text-base font-body font-semibold leading-6 text-gray-900">
+                State
+              </label>
+              <Select
+                id="state"
+                name="state"
+                options={updatedStates(values.country.isoCode)}
+                value={values.state}
+                onChange={handleStateChange}
+              />
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col w-full">
+          {values.state && (
+            <div>
+              <label className="block text-base font-body font-semibold leading-6 text-gray-900">
+                City
+              </label>
+              <Select
+                id="city"
+                name="city"
+                options={updatedCities(
+                  values.country.isoCode,
+                  values.state.isoCode
+                )}
+                value={values.city} // Use values.city instead of city
+                onChange={handleCityChange}
+              />
+            </div>
+          )}
+        </div>
       </form>
+      <button
+        className="w-full bg-primary text-white font-body font-semibold py-1 rounded-full mt-10"
+        type="submit"
+      >
+        Save
+      </button>
     </motion.div>
   );
 };
