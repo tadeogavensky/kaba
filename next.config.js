b5cd2ae6ce82fd21cbcfe566e1e2ca30373a1e23
tadeogavensky <tadeogavensky.email@gmail.com>
2023-10-17 22:43:23 -0300

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
- images: {
+  images: {
     remotePatterns: [
       {
         protocol: "https",
@@ -12,10 +12,31 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/(.*)',
-        destination: 'https://main--kabax.netlify.app/api/(.*)',
+        source: "/api/:path*",
+        destination: "https://main--kabax.netlify.app/api/:path*",
       },
-    ]
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://main--kabax.netlify.app/" }, 
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
