6cfa9c2de77b428760dff909683823a9d3f22e30
tadeogavensky <tadeogavensky.email@gmail.com>
2023-11-01 23:43:30 -0300

    changes

--- components/home/AlertWorker.tsx
+++ components/home/AlertWorker.tsx
@@ -30,6 +30,9 @@ const AlertWorker = () => {
 
   return (
     <div className="mt-6">
+      <h1 className="font-body text-lg text-center">
+        Your active service <span className="font-bold">{user?.worker?.service?.name}</span>
+      </h1>
       {alertMessages.length > 0 && (
         <div className="text-center font-body font-semibold text-xs bg-primary text-white shadow-lg p-6 rounded-lg">
           Hi {user?.firstName}, to be visible for our Kaba Clients, please:
